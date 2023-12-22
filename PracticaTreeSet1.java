55b99dc5a84e9a3f404f093ac7e3adfb1afd1100
Su Shan <131684159+SuVoong@users.noreply.github.com>
2023-12-22 23:56:56 +0100

    segundo commit

---
+++ PracticaTreeSet1.java
@@ -0,0 +1,31 @@
+package practicaTreeSet1;
+
+
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+public class PracticaTreeSet1 {
+	public static void main (String [] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		TreeSet<Integer> num = new TreeSet<Integer>();
+		System.out.println("Introduce el número");
+		String numero[] = sc.nextLine().split(" ");
+		
+		for(String strNumero: numero ) {
+			try {
+				if(!num.add(Integer.parseInt(strNumero))) {
+					System.out.println("El número "+ strNumero + " esta repetido");
+				}
+			}catch(NumberFormatException e){
+//				System.out.println("Error " + e);
+			}
+		}
+		System.out.println("Los números ordenados ascendientemente y sin repetidos son: ");
+		Iterator<Integer> iterator = num.iterator();
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next() + " ");
+		}
+	}
+}
+# Java-Mapas-1
